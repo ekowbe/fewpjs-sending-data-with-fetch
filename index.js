da1e34608eaa1d19fc4185cab9d82bd3d2d05606
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-07-26 14:22:04 +0000

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email,
+      };
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    fetch("http://localhost:3000/users", configObj)
+        .then(resp => {
+            return resp.json();
+        })
+        .then(json => {
+            console.log(json.id)
+            document.body.innerHTML += `${json.id}`
+        })
+        .catch(function(error) {
+            alert("serious!")
+            console.log(error.message)
+            document.body.innerHTML += `${error.message}`
+        });
+
+}
+
+
+  
